b4525e61987e0ea84db141c32ad8140a7617985b
tiaanvanrooyen <tiaanvrfrederik@gmail.com>
2026-03-27 23:10:40 +0200

    initial commit

--- src/test/java/com/example/tooth_fairy/ToothFairyApplicationTests.java
+++ src/test/java/com/example/tooth_fairy/ToothFairyApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.tooth_fairy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ToothFairyApplicationTests {
 
 	@Test
